12c6e6b96e42050a55f3691fe3542bd2c9e2349f
Jacob Cunningham <jcun4128@gmail.com>
2023-09-23 12:47:49 -0500

    struggling

--- DEVLOG.md
+++ DEVLOG.md
@@ -3,3 +3,18 @@
 11:58 AM
 
 struggling to work on this for some reason, fresh brain
+
+12:25 PM
+
+can see how the API pulls the values from GS
+
+<img src="./pull-vals-from-gs.jpg"/>
+
+12:42 PM
+
+reeeeeee scrollin social media like a dumbass
+
+free yourself boy
+
+- [ ] replace bills with card values from net worth
+
--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,67 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
+const apiBase = 'http://192.168.1.144:5045'; // use an env file, enum, etc...
+const apiPathGetNetWorth = `${apiBase}/get-latest-net-worth`;
+const apiPathGetBills = `${apiBase}/get-bills`;
+const apiPathGetCards = `${apiBase}/get-cards`;
+
+const getNetWorth = () => {
+  return new Promise (async (resolve, reject) => {
+    try {
+      const response = await fetch(apiPathGetNetWorth);
+      const netWorth = await response.json();
+      console.log('net worth', netWorth);
+      resolve(netWorth);
+    } catch (err) {
+      reject(err);
+    }
+  });
+}
+
+const getBills = () => {
+  return new Promise (async (resolve, reject) => {
+    try {
+      const response = await fetch(apiPathGetBills);
+      const bills = await response.json();
+      console.log('bills', bills);
+      resolve(bills);
+    } catch (err) {
+      reject(err);
+    }
+  });
+}
+
+const getCards = async () => {
+  return new Promise (async (resolve, reject) => {
+    try {
+      const response = await fetch(apiPathGetCards);
+      const cards = await response.json();
+      console.log('cards', cards);
+      resolve(cards);
+    } catch (err) {
+      reject(err);
+    }
+  });
+}
+
+const filterVals = async () => {
+  const netWorth = await getNetWorth();
+  const bills = await getBills();
+  const cards = await getCards();
+}
+
 function App() {
+  const [appData, setAppData] = useState({
+    netWorth: {},
+    cards: {},
+    bills: {},
+  });
+
+  useEffect(() => {
+    filterVals();
+  }, []);
+
   return (
     <div className="App">
       
